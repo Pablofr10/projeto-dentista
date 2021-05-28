217ad894f2d082c95d13beef3f78becc3c170ea9
Pablo Araújo <rafanet.live@gmail.com>
2021-05-28 00:22:25 -0300

    Finalizando estrutura

---
+++ dentista-backend/src/Dentista.Core/Interfaces/Services/IPacienteService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dentista.Core.Entities;
+
+namespace Dentista.Core.Interfaces.Services
+{
+    public interface IPacienteService
+    {
+        Task<IEnumerable<Paciente>> Get();
+        Task<Paciente> Get(int idPaciente);
+    }
+}
