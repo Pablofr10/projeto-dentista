b6923a48ec36ad548a2322efe8047a250af52459
Pablo Araújo <rafanet.live@gmail.com>
2021-05-28 22:37:12 -0300

    Adicionando DbContext

--- dentista-backend/src/Dentista.Infrastructure/Commom/DentistaDbContext.cs
+++ dentista-backend/src/Dentista.Infrastructure/Commom/DentistaDbContext.cs
@@ -1,7 +1,31 @@
-﻿namespace Dentista.Infrastructure.Commom
+﻿using Dentista.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dentista.Infrastructure.Commom
 {
-    public class DentistaDbContext
+    public sealed class DentistaDbContext : DbContext
     {
-        
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+        public DbSet<Procedimento> Procedimentos { get; set; }
+        public DbSet<Profissional> Profissionais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        public DentistaDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DentistaDbContext(DbContextOptions<DentistaDbContext> options) : base(options)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
     }
 }
