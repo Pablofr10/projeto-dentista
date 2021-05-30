c97026af3448a341bd859939173521eb6e89a135
Pablo Araújo <rafanet.live@gmail.com>
2021-05-30 16:36:20 -0300

    Adicionando consulta

---
+++ dentista-backend/src/Dentista.Core/Interfaces/Repositories/IConsultaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dentista.Core.Entities;
+
+namespace Dentista.Core.Interfaces.Repositories
+{
+    public interface IConsultaRepository : IBaseRepository
+    {
+        Task<IEnumerable<Consulta>> BuscarConsultas();
+        Task<Consulta> BuscarConsulta(int IdPaciente);
+    }
+}
---
+++ dentista-backend/src/Dentista.Infrastructure/Repositories/ConsultaRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Dentista.Core.DTOs;
+using Dentista.Core.Interfaces.Services;
+using Dentista.Infrastructure.Commom;
+
+namespace Dentista.Infrastructure.Repositories
+{
+    public class ConsultaRepository : BaseRepository, IConsultaService
+    {
+        public ConsultaRepository(DentistaDbContext context) : base(context)
+        {
+        }
+
+        public Task<ConsultaDto> BuscarConsultas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ConsultaDto> BuscarConsulta()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> MarcarConsulta()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AtualizarConsulta()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
