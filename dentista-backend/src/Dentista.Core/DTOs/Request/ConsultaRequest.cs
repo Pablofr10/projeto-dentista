00fd718967bd33778fa4db896915d276ddad760b
Pablo Araújo <rafanet.live@gmail.com>
2021-06-02 23:32:04 -0300

    Adicionando marcacao de consulta

---
+++ dentista-backend/src/Dentista.Core/DTOs/Request/ConsultaRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Dentista.Core.Entities;
+
+namespace Dentista.Application.Request
+{
+    public class ConsultaRequest
+    {
+        public DateTime DataConsulta { get; set; }
+        public int Status { get; set; }
+        public int PagamentoId { get; set; }
+        public List<int> Especialidades { get; set; }
+        public int ProfissionalId { get; set; }
+        public int PacienteId { get; set; }
+    }
+}
