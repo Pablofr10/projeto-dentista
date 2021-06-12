b2d1d6ef6dffe0c98077a4f1800d9356c0f02744
Pablo Rafael <rafanet.live@gmail.com>
2021-06-12 20:20:53 -0300

    Criando service para profissional

--- dentista-backend/src/Dentista.Application/Services/ProfissionalService.cs
+++ dentista-backend/src/Dentista.Application/Services/ProfissionalService.cs
@@ -1,7 +1,35 @@
-﻿namespace Dentista.Application.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dentista.Core.DTOs;
+using Dentista.Core.Interfaces.Services;
+
+namespace Dentista.Application.Services
 {
-    public class ProfissionalService
+    public class ProfissionalService : IProfissionalService
     {
-        
+        public Task<IEnumerable<ProfissionalDto>> BuscarProfissionais()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ProfissionalDto> BuscarProfissional(int idProfissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AdicionarProfissional(ProfissionalDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AtualizarProfissional(int idProfissional, ProfissionalDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AdicionarEspecialidade(EspecialidadeDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
