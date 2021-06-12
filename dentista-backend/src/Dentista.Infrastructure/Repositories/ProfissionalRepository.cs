d4184d7e2a98373b6bcc466d7dd6d09c9603a984
Pablo Rafael <rafanet.live@gmail.com>
2021-06-12 20:23:11 -0300

    Adicionando repository

---
+++ dentista-backend/src/Dentista.Infrastructure/Repositories/ProfissionalRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dentista.Core.DTOs;
+using Dentista.Core.Interfaces.Services;
+using Dentista.Infrastructure.Commom;
+
+namespace Dentista.Infrastructure.Repositories
+{
+    public class ProfissionalRepository : BaseRepository, IProfissionalService
+    {
+        public ProfissionalRepository(DentistaDbContext context) : base(context)
+        {
+        }
+
+        public Task<IEnumerable<ProfissionalDto>> BuscarProfissionais()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ProfissionalDto> BuscarProfissional(int idProfissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AdicionarProfissional(ProfissionalDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AtualizarProfissional(int idProfissional, ProfissionalDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AdicionarEspecialidade(EspecialidadeDto profissional)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
