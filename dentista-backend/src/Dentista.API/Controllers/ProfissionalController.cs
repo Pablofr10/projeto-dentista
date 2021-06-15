96323a9b4a37f79dc63fde075f8adea263270625
Pablo Rafael <rafanet.live@gmail.com>
2021-06-14 23:12:20 -0300

    Adicionando endpoint atualizar e adicionar na controller

--- dentista-backend/src/Dentista.API/Controllers/ProfissionalController.cs
+++ dentista-backend/src/Dentista.API/Controllers/ProfissionalController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Dentista.Core.DTOs;
 using Dentista.Core.Interfaces.Services;
 using Dentista.Core.Params;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,26 @@ namespace Dentista.API.Controllers
             if (profissional != null) return Ok(profissional);
 
             return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PutProfissional(ProfissionalDto profissional)
+        {
+            var profissionalAdicionado = await _service.AdicionarProfissional(profissional);
+
+            return profissionalAdicionado
+                ? Ok("Profissional atualizado")
+                : BadRequest("Erro ao atualizar profissional");
+        }
+
+        [HttpPut("{idProfissional}")]
+        public async Task<IActionResult> PutProfissional(int idProfissional, ProfissionalDto profissional)
+        {
+            var profissionalAtualizado = await _service.AtualizarProfissional(idProfissional, profissional);
+
+            return profissionalAtualizado
+                ? Ok("Profissional atualizado")
+                : BadRequest("Erro ao atualizar profissional");
         }
     }
 }
