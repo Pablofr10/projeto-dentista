f230455efe0b07e1b8f64d6cf33bb43ac5ec6ab8
Pablorf10 <rafanet.live@gmail.com>
2021-07-16 23:26:49 -0300

    Criando lógica para autenticação.

---
+++ dentista-frontend/components/autenticacao/Cadastro.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-card-title class="text-h4 text--primary">
+      Cadastro
+    </v-card-title>
+
+    <v-card-text>
+      <v-text-field label="Usuário" />
+      <v-text-field label="Senha" />
+      <v-text-field label="Confirmar Senha" />
+      <v-btn block color="primary">
+        Cadastrar
+      </v-btn>
+    </v-card-text>
+  </div>
+</template>
+<script>
+export default {}
+</script>
--- dentista-frontend/pages/autenticacao.vue
+++ dentista-frontend/pages/autenticacao.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>
-    <Login />
-  </div>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+    elevation="5"
+  >
+    <template slot="progress">
+      <v-progress-linear color="deep-purple" height="10" indeterminate />
+    </template>
+    <Login v-if="isLogin" />
+    <Cadastro v-else />
+
+    <v-card-actions>
+      <v-btn text color="teal accent-4" @click="isLogin = !isLogin">
+        {{ isLogin ? 'cadastrar' : 'login' }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
 export default {
-  layout: 'empty'
+  layout: 'empty',
+  data () {
+    return {
+      isLogin: true
+    }
+  }
 }
 </script>
