138bce840fb38b9a60483ab9e0a6d0ca2a67271b
Pablorf10 <rafanet.live@gmail.com>
2021-08-17 21:35:36 -0300

    Adicionando talwind no projeto

--- dentista-frontend/src/main.js
+++ dentista-frontend/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
