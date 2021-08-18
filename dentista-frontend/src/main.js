447a74cc03ca16a96eace1cd7f88dfa19ab00e94
Pablorf10 <rafanet.live@gmail.com>
2021-08-17 21:37:03 -0300

    Adicionando rotas

--- dentista-frontend/src/main.js
+++ dentista-frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import './assets/tailwind.css'
+import router from './router'
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
