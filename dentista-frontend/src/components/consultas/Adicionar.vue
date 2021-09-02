a0128e98b9737f2637e756c5624b4d114e46ca78
Pablorf10 <rafanet.live@gmail.com>
2021-09-02 20:41:04 -0300

    Informações do usuário.

--- dentista-frontend/src/components/consultas/Adicionar.vue
+++ dentista-frontend/src/components/consultas/Adicionar.vue
@@ -51,145 +51,48 @@
                       <dd
                         class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
                       >
-                        Margot Foster
+                        {{ informacoesPaciente.nome }}
                       </dd>
                     </div>
                     <div
                       class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
                     >
                       <dt class="text-sm font-medium text-gray-500">
-                        Application for
+                        WhatsApp
                       </dt>
                       <dd
                         class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
                       >
-                        Backend Developer
+                        {{ informacoesPaciente.telefone }}
                       </dd>
                     </div>
                     <div
                       class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
                     >
                       <dt class="text-sm font-medium text-gray-500">
-                        Email address
+                        Total de Consultas
                       </dt>
                       <dd
                         class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
                       >
-                        margotfoster@example.com
+                        {{ informacoesPaciente.consultasFinalizadas }}
                       </dd>
                     </div>
                     <div
                       class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
                     >
                       <dt class="text-sm font-medium text-gray-500">
-                        Salary expectation
+                        Paciente Ativo
                       </dt>
                       <dd
                         class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
                       >
-                        $120,000
+                        {{ informacoesPaciente.ativo ? "Sim" : "Não" }}
                       </dd>
                     </div>
                   </dl>
                 </div>
-                <label
-                  for="about"
-                  class="block text-sm font-medium text-gray-700"
-                >
-                  About
-                </label>
-                <div class="mt-1">
-                  <textarea
-                    id="about"
-                    name="about"
-                    rows="3"
-                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                    placeholder="you@example.com"
-                  />
-                </div>
-                <p class="mt-2 text-sm text-gray-500">
-                  Brief description for your profile. URLs are hyperlinked.
-                </p>
               </div>
-
-              <div>
-                <label class="block text-sm font-medium text-gray-700">
-                  Photo
-                </label>
-                <div class="mt-1 flex items-center">
-                  <span
-                    class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100"
-                  >
-                    <svg
-                      class="h-full w-full text-gray-300"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
-                      />
-                    </svg>
-                  </span>
-                  <button
-                    type="button"
-                    class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Change
-                  </button>
-                </div>
-              </div>
-
-              <div>
-                <label class="block text-sm font-medium text-gray-700">
-                  Cover photo
-                </label>
-                <div
-                  class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
-                >
-                  <div class="space-y-1 text-center">
-                    <svg
-                      class="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                    <div class="flex text-sm text-gray-600">
-                      <label
-                        for="file-upload"
-                        class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                      >
-                        <span>Upload a file</span>
-                        <input
-                          id="file-upload"
-                          name="file-upload"
-                          type="file"
-                          class="sr-only"
-                        />
-                      </label>
-                      <p class="pl-1">or drag and drop</p>
-                    </div>
-                    <p class="text-xs text-gray-500">
-                      PNG, JPG, GIF up to 10MB
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-              <button
-                type="submit"
-                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Save
-              </button>
             </div>
           </div>
         </form>
@@ -208,7 +111,7 @@
       <div class="md:col-span-1">
         <div class="px-4 sm:px-0">
           <h3 class="text-lg font-medium leading-6 text-gray-900">
-            Personal Information
+            Informações da Consulta
           </h3>
           <p class="mt-1 text-sm text-gray-600">
             Use a permanent address where you can receive mail.
@@ -511,7 +414,7 @@
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
+import { reactive, ref, toRefs } from "@vue/reactivity";
 import service from "../../service/index";
 
 export default {
@@ -532,12 +435,14 @@ export default {
     }
 
     async function buscarDadosPaciente() {
-      const { dados } = await service.get(`paciente/${codigoPaciente.value}`);
-      dadosPaciente.informacoesPaciente = dados;
+      dadosPaciente.informacoesPaciente = [];
+      const { data } = await service.get(`paciente/${codigoPaciente.value}`);
+      dadosPaciente.informacoesPaciente = data;
+      console.log(dadosPaciente.informacoesPaciente);
     }
 
     return {
-      ...dadosPaciente,
+      ...toRefs(dadosPaciente),
       buscarPaciente,
       codigoPaciente
     };
